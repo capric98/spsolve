e4563bed3f6f1cca2f1fbb3b6ecbda4d8b75d315
capric98 <42015599+capric98@users.noreply.github.com>
2025-07-23 00:10:16 -0500

    psutil cpu_affinity() not available in mac

--- src/spsolve/spparms.py
+++ src/spsolve/spparms.py
@@ -1,10 +1,15 @@
-import os
-import psutil
-
 _PREFER_ORDER    = "C"
-_PS_CPU_AFFINITY = psutil.Process().cpu_affinity()
-_PS_CPU_AFFINITY = [0] if not _PS_CPU_AFFINITY else _PS_CPU_AFFINITY
-_OMP_NUM_THREADS = int(os.getenv("OMP_NUM_THREADS", len(_PS_CPU_AFFINITY)))
+_OMP_NUM_THREADS = 0 # use auto detect in C++ by default
+
+
+try:
+    import os
+    import psutil
+    _PS_CPU_AFFINITY = psutil.Process().cpu_affinity()
+    _PS_CPU_AFFINITY = [0] if not _PS_CPU_AFFINITY else _PS_CPU_AFFINITY
+    _OMP_NUM_THREADS = int(os.getenv("OMP_NUM_THREADS", len(_PS_CPU_AFFINITY)))
+except Exception as _:
+    pass
 
 
 def set_num_threads(n: int):
